b67e2823e7257321a866909df75a023a42e65e6f
Recode <recodeexistance@gmail.com>
2020-11-01 11:39:38 +0000

    file and folder housekeeping.

---
+++ Algorithms/MultiplePointers/multiplePointers.js
@@ -0,0 +1,13 @@
+function sumsToTen(arrNumList) {
+    for(let i = 0 ; i < arrNumList.length; i++) {
+        let currentNum = arrNumList[i]; 
+        for(let j = i+1 ; j < arrNumList.length ; j++) {
+            if (currentNum + arrNumList[j] === 10) {
+                return 'Match found'; 
+            }
+        }
+    }
+}
+
+sumsToTen([1,2,3,4,5,6,7,8,9]); 
+
