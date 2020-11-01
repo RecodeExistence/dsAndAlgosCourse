26bfd4e122d170c651bd295a1ec5bf928690e5da
Recode <recodeexistance@gmail.com>
2020-11-01 11:51:47 +0000

    added multiple pointers explanation/description

---
+++ Algorithms/MultiplePointers/MutiplePointers-Explanation.js
@@ -0,0 +1,14 @@
+
+
+// Multiple pointers Algorithmn.  Best explained by demonstration.   
+
+// This must be a SORTED array before acting on it. 
+
+//Multiple pointers refers to the ability to create one or more references to positions of a linear structure, such as a string, array, and other more advanced structures such as doubly or singly linked lists, and them move those pointers base upon the outcome of a condition. Each move of a pointer we are checking for a particular outcome, such as a pair,  or we could use this as another version of a frequency counter patterns return outcome. 
+
+// In this example, we use a reference at the start and at the end of the linear structure and then work towards the middle towards one another.  When the term references is used, it is referring to variables, such as i and j, or pointer1 and pointer2, with values as indexes we are pointing at in the linear structure, such as an index of an array. ie a location in an arrar, or a location in a string etc.  
+// We could also work from the end of the structure towards the beginning, or the middle to either end.  
+
+// Essentially, we are returning  result of a comparisson, or count etc , the value of that result arriving from the pointers moving in a direction pre-defined or based upon a condition, and carrying out those comparissons or count et al.  
+
+// It is also important to remember that the references are specifically references to data locations in memory such as a variable with an array stored in it. 
--- Algorithms/MultiplePointers/multiplePointers.js
+++ Algorithms/MultiplePointers/multiplePointers.js
@@ -11,3 +11,5 @@ function sumsToTen(arrNumList) {
 
 sumsToTen([1,2,3,4,5,6,7,8,9]); 
 
+
+
