d0f267cf05b1e9aafc4941dd97856d3d265b8886
Recode <recodeexistance@gmail.com>
2020-12-06 09:37:19 +0000

    I have two workable versions, with known bug.  End of array problem, see notes.

--- ztmCourseAlgo/ztm-mergeSortedArrays.js
+++ ztmCourseAlgo/ztm-mergeSortedArrays.js
@@ -1,36 +1,23 @@
 function createRandomNums() {
     let arrNums = []; 
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 3; i++) {
         arrNums.push(Math.floor(Math.random() * 100)); 
     }
     return arrNums;
 }
 
 
-function mergeSortedNumbers(vArrayOfNumbers1, vArrayOfNumbers2) {
-    let iArray1Pointer = 0; 
-    let iArray2Pointer = 0; 
-
-    let arrMergedNumberArrays = []; 
-    let bReachedEndOfArray = false; 
-
-    do {
-        if(iArray1Pointer === vArrayOfNumbers1.length || iArray2Pointer === vArrayOfNumbers2.length) {
-            bReachedEndOfArray = true; 
-        }
-        if(vArrayOfNumbers1[iArray1Pointer] < vArrayOfNumbers2[iArray2Pointer]) {
-            arrMergedNumberArrays.push(vArrayOfNumbers1[iArray1Pointer]); 
-            iArray1Pointer++; 
-        } else {
-            arrMergedNumberArrays.push(vArrayOfNumbers2[iArray2Pointer]); 
-            iArray2Pointer++; 
-            } 
-        }
-    while(bReachedEndOfArray === false);
-	return arrMergedNumberArrays; 
-}
 
 
 let arr1 = createRandomNums(); 
 let arr2 = createRandomNums(); 
 console.log(mergeSortedNumbers(arr1, arr2));
+
+
+               
+// 1, 20, 40, 79
+
+// 2, 7, 15, 100
+// 4 is greater than 1, j++, 4 is greater than 2, j++, 4 is greater than 3 j++, 
+// 4 is less than 5 so arr1[current index minus 1] is 4.  
+
