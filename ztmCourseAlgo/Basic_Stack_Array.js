6d87cc5b805fb956d3591172c59ac7493657b11f
Recode <recodeexistance@gmail.com>
2021-02-24 21:29:29 +0000

    Added value parameter for push

--- ztmCourseAlgo/Basic_Stack_Array.js
+++ ztmCourseAlgo/Basic_Stack_Array.js
@@ -1,5 +1,11 @@
-// Implementing a stack in an array in Javascript is very simple, as arrays come with 
-// predefined methods to make our life easier, including length, push and pop.  
+/* Implementing a stack in an array in Javascript is very simple, as arrays come with 
+ predefined methods to make our life easier, including length, push and pop.
+
+ The trade of here of course is performance.  Easy set up, but O(N) time complexity 
+ on the pop, as we'd have to re-index the array.  
+Also if the value we're pushing makes the array longer than it's already dimmed length, 
+ we have to double up the array length in memory before we can push to it, so 
+space complexity can be an issue.  */
 
 class Stack {
     constructor() {
